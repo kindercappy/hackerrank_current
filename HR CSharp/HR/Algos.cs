66a6592f9f096d3b81728124ed9c81923ccf512e
Kinder <kinderpreet21@outlook.com>
2018-02-22 21:29:21 +0530

    12

--- HR CSharp/HR/Algos.cs
+++ HR CSharp/HR/Algos.cs
@@ -10,10 +10,12 @@ namespace HR
     {
         static void Main(string[] args)
         {
-            int n = 5;
             string[] arr_temp = Console.ReadLine().Split(' ');
-            int[] arr = { 1, 0, 5, 7, 4, 12 };
-            CorrectnessAndTheLoopInvariant(arr);
+            int[] arr = { 99, 0, 99, 100, 4, 12, 123, 2, 3, 1, 3, 1232 };
+            //int[] arr = { 2, 10, 3, 7, 9, 4, 6, 12, 8 };
+
+            int[] result = CountingSort(arr);
+            Console.WriteLine(String.Join(" ", result));
             Console.ReadLine();
         }
         public static void InsertionSort1(int n, int[] arr)
@@ -55,18 +57,17 @@ namespace HR
                     }
                     putBackIndex += 1;
                 }
-            Console.WriteLine(string.Join(" ", arr));
+                Console.WriteLine(string.Join(" ", arr));
             }
             Console.ReadLine();
         }
-
         private static int IntroTutorial(int V, int[] arr)
         {
             // Complete this function
             int result = 0;
             double mid = arr.Length / 2;
-            int center =(int) Math.Round(mid);
-            for(int i =0; i < arr.Length; i += 1)
+            int center = (int)Math.Round(mid);
+            for (int i = 0; i < arr.Length; i += 1)
             {
                 if (arr[i] == V)
                 {
@@ -75,7 +76,6 @@ namespace HR
             }
             return result;
         }
-
         private static void CorrectnessAndTheLoopInvariant(int[] A)
         {
             var j = 0;
@@ -91,6 +91,48 @@ namespace HR
                 A[j + 1] = value;
             }
             Console.WriteLine(string.Join(" ", A));
+        }
+        private static int[] QuickSort(int[] arr)
+        {
+            int[] sorted = new int[arr.Length];
+            int[] pivot = new int[] { arr[0] };
+            int[] left = new int[0];
+            int[] right = new int[0];
+            int insertLeft = 0;
+            int insertRight = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < pivot[0])
+                {
+                    if (insertLeft == left.Length) Array.Resize(ref left, left.Length+1);
+                    left[insertLeft] = arr[i];
+                    insertLeft++;
+                }
+                else if (arr[i] > pivot[0])
+                {
+                    if (insertRight == right.Length) Array.Resize(ref right, right.Length+1);
+                    right[insertRight] = arr[i];
+                    insertRight++;
+                }
+            }
+            left = left.Concat(pivot).ToArray();
+            sorted = left.Concat(right).ToArray();
+            return sorted;
+        }
+        private static int[] CountingSort(int[] arr)
+        {
+            int[] result = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                int num = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i==arr[j])num++;
+                }
+                result[i] = num;
+            }
+            return result;
         }
     }
 }
